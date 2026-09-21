52b55e60f133c956f3a709bc74f4131e0b9aa2c2
Yahia Abdel Rahman Mad'd Fares <yahyafaris2004y@gmail.com>
2026-09-21 05:29:54 +0300

    Update HTML message in setup-server.sh

--- setup-server.sh
+++ setup-server.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Update package list
 sudo apt-get update -y
+
+# Install Nginx
 sudo apt-get install nginx -y
+
+# Start Nginx service
 sudo systemctl start nginx
+
+# Enable Nginx to start on boot
 sudo systemctl enable nginx
-echo "<h1>Hello from my first Automated AWS Server!</h1>" | sudo tee /var/www/html/index.html
+
+# Create a simple HTML page
+echo "<h1>Hello from my automated EC2 server!</h1>" | sudo tee /var/www/html/index.html
